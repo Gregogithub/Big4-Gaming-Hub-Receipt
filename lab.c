900c183508a2b6e01478503694aeb7434714a58f
Gregogithub <127564665+Gregogithub@users.noreply.github.com>
2023-04-04 17:00:48 +0300

    Add files via upload

---
+++ lab.c
@@ -0,0 +1,406 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+
+{
+int numberofgamesplayed;
+int player,x,totalamount;
+x=30;
+printf("WELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+//next
+printf("\n\nWELCOME TO BIG FOUR GAMING HUB 2023");
+printf("\nLIPA NA MPESA\nPOCHI LA BIASHARA\n0794809603");
+printf("\nENTER PLAYER(USE NUMBERS):");
+scanf("%d",&player);
+printf("ENTER NUMBER OF GAMES PLAYED: ");
+scanf("%d",&numberofgamesplayed);
+if(numberofgamesplayed>=5)
+{
+printf("Player %d will pay:%d",player,numberofgamesplayed*x-(30));
+}
+else if(numberofgamesplayed<5)
+{
+printf("Player %d will pay:%d",player, numberofgamesplayed*x);
+}
+printf("\nWELCOME BACK ANOTHER DAY");
+printf("\n**************END********************");
+printf("\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+return 0;
+}
+
